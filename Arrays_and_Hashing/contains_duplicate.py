5f4f547ed9f6f623a509da6b612e4d2623cca57f
Dhanush Gowda H K <dhanushgowdahk04@gmail.com>
2026-07-01 22:46:29 +0530

    Solve Contains Duplicate

---
+++ Arrays_and_Hashing/contains_duplicate.py
@@ -0,0 +1,19 @@
+# leetcode 217 : https://leetcode.com/problems/contains-duplicate/
+
+def containsDuplicat(nums):
+    num_set = set()
+
+    for n in nums:
+        if n in num_set:
+            return True
+        num_set.add(n)
+    return False
+
+# Example usage:
+nums = [1, 2, 3, 1]
+result = containsDuplicat(nums)
+print(result)  # Output: True
+
+# Time Complexity: O(N), where N is the number of elements in the input list. We iterate through the list once, and each lookup in the set takes O(1) time on average.
+# Space Complexity: O(N), where N is the number of elements in the input list.
+
